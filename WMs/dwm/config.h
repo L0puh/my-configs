e833ce7cc82adbefd108505ab044b8214d1fe1f6
L0puh <1opuhart@gmail.com>
2023-10-29 18:33:24 +0900

    screenshots

--- WMs/dwm/config.h
+++ WMs/dwm/config.h
@@ -61,14 +61,18 @@ static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() 
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "st", NULL };
 static const char *browser[] =  {"firefox", NULL};
+static const char *screenshot[] =  {"flameshot", "screen", NULL};
+static const char *screengui[] =  {"flameshot", "gui", NULL};
 
 static const Key keys[] = {
 	/* modifier                     key        function        argument */
 
-	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
-	{ MODKEY,			XK_F1, 	   spawn, 	   {.v = browser }},
-	{ MODKEY,             		XK_Return, spawn,          {.v = termcmd } },
-	{ MODKEY,                       XK_b,      togglebar,      {0} },
+	{ MODKEY,                       XK_p,       spawn,          {.v = dmenucmd } },
+	{ MODKEY,			            XK_F1, 	    spawn, 	        {.v = browser  } },
+	{ MODKEY,             		    XK_Return,  spawn,          {.v = termcmd  } },
+	{ MODKEY,             		    XK_F2,      spawn,          {.v = screenshot } },
+	{ MODKEY,             		    XK_F3,      spawn,          {.v = screengui} },
+	{ MODKEY,                       XK_b,      togglebar,      {1} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
 	{ MODKEY,                       XK_i,      incnmaster,     {.i = +1 } },
