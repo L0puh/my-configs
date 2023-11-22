c4f139950ce4a5612ff15bc9aaf3152f2aa65536
L0puh <1opuhart@gmail.com>
2023-11-22 19:08:00 +0900

    script

--- scripts/book.py
+++ scripts/book.py
@@ -9,6 +9,9 @@ def get_dir():
         directory = os.environ["HOME"] + "/Documents/books"
     print("found directory: ", directory)
     return directory
+def get_name(book):
+    name = book.replace(".pdf", "").replace("_", " ").replace("-", " ").lower()
+    return name 
 
 def main():
     books=[]
@@ -16,10 +19,20 @@ def main():
     print("BOOKS:")
     for num, book in enumerate(os.listdir(dirc), 1):
         books.append(book)
-        print(num, ": ", book)
-    choice = int(input("> "))
-    if choice <= len(books):
-        os.system(f"zathura {dirc}/{books[choice-1]}")
+        print(num, ": ", get_name(book))
+    choice = (input("> "))
+    if choice.isdigit():
+        if int(choice) <= len(books):
+            os.system(f"zathura {dirc}/{books[int(choice)-1]}")
+    else: 
+        for book in books:
+            name = get_name(book).split(" ")
+            for i in name:
+                if i == choice:
+                    os.system(f"zathura {dirc}/{book}")
+                    return
+
+
     
      
 
