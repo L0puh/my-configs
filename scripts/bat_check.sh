7f1dc29c5ea9a1357f5d2cce41ca83b5aad4c5db
L0puh <1opuhart@gmail.com>
2024-03-09 11:58:48 +0900

    battery

---
+++ scripts/bat_check.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# in crontab:
+# 15 * * * * /path/to/bat_check.sh 
+
+BATTINFO=`acpi -b`
+if [[ `echo $BATTINFO | grep Discharging` && `echo $BATTINFO | cut -f 4 -d " " | cut -c 1-2` < 15 ]] ; then
+    DISPLAY=:0.0 /usr/bin/xmessage -print LOW BATTERY: $BATTINFO"!" -bg "#121212" -fg "#d4cfd0"
+fi
